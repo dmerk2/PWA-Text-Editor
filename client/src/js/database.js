f30cbe386a4c8e4251ac4a3a521ded70bc07fe7f
dmerk2 <dan.merkin@gmail.com>
2023-11-08 22:31:38 -0500

    Registering route for service-worker to configure

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,61 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    // Create connection to the database and the version
+    const jateDb = await openDB("jate", 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+    // Create new trasaction and data privileges
+    const tx = jateDb.transaction("jate", "readwrite");
+
+    // Open the object store
+    const store = tx.objectStore("jate");
+
+    // Add the stored data
+    const request = store.add({ content });
+
+    const result = await request;
+    console.log("Data saved to the database", result);
+  } catch (error) {
+    console.error("Could not save to database:", error);
+  }
+};
+
+export const getDb = async () => {
+  try {
+    // Create connection to the database and the version
+    const jateDb = await openDB("jate", 1);
+
+    // Create new trasaction and data privileges
+    const tx = jateDb.transaction("jate", "readonly");
+
+    // Open the object store
+    const store = tx.objectStore("jate");
+
+    // getAll DOES NOT WORK
+
+    const request = store.getAll();
+
+    // get(1) DOES WORK
+    // const request = store.get(1);
+    const result = await request;
+    console.log("Results:", result);
+    return result;
+  } catch (error) {
+    console.error("Could not GET database:", error);
+  }
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: "development",
@@ -32,18 +29,18 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Just Another Text Editer',
-        short_name: 'JATE',
-        description: 'Your personal text editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "Just Another Text Editer",
+        short_name: "JATE",
+        description: "Your personal text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
@@ -53,21 +50,29 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           // We use babel-loader in order to use ES6.
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
       ],
+    },
+    devServer: {
+      client: {
+        overlay: false,
+      },
     },
   };
 };
